884a04186b16409578317d1bb39307525db8ea1b
kche0169 <shiratakekanpakuji@gmail.com>
2026-04-26 08:46:56 +0800

    feat(store): 添加基础事件发射器实现

---
+++ public/js/store/emitter.js
@@ -0,0 +1,28 @@
+/**
+ * Simple EventEmitter for state change notifications
+ */
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+    return () => this.off(event, callback);
+  }
+
+  off(event, callback) {
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event].filter(cb => cb !== callback);
+  }
+
+  emit(event, data) {
+    if (!this.events[event]) return;
+    this.events[event].forEach(callback => callback(data));
+  }
+}
+
+export const emitter = new EventEmitter();
